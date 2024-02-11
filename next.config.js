4283f7be92216deca3ff53bbb800fbf391f4c900
Bohdan Garashchuk <bohdan.garashchuk@gmail.com>
2024-02-11 02:38:41 +0200

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     compiler: {
-      styledComponents: true
-    }
+      styledComponents: true,
+    },
   };
 
 module.exports = nextConfig
